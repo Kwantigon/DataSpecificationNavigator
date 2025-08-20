a3caf4d6c7cd1b4b0c4eb3950e1215f9d69827eb
Quoc Anh Nguyen <nquocanh97@gmail.com>
2025-08-20 14:10:59 +0200

    Commit code for frontend and backend.

---
+++ backend/DataSpecificationNavigatorBackend/BusinessCoreLayer/Abstraction/IDataSpecificationService.cs
@@ -0,0 +1,13 @@
+﻿using DataSpecificationNavigatorBackend.Model;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DataSpecificationNavigatorBackend.BusinessCoreLayer.Abstraction;
+
+public interface IDataSpecificationService
+{
+	Task<DataSpecification?> ExportDataSpecificationFromDataspecerAsync(string dataspecerPackageUuid, string dataspecerPackageName);
+
+	Task<List<DataSpecificationItem>> GetDataSpecificationItemsAsync(
+		int dataSpecificationId,
+		List<string> itemIriList);
+}
---
+++ backend/DataSpecificationNavigatorBackend/ConnectorsLayer/Abstraction/IDataspecerConnector.cs
@@ -0,0 +1,7 @@
+﻿namespace DataSpecificationNavigatorBackend.ConnectorsLayer.Abstraction;
+
+public interface IDataspecerConnector
+{
+	Task<string?> ExportDsvFileFromPackageAsync(string packageIri);
+	Task<string?> ExportOwlFileFromPackageAsync(string packageIri);
+}
