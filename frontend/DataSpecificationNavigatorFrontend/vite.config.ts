435e58cea1b8eb7754b3897f16e0c0186f435ba0
Quoc Anh Nguyen <nguyen@ica.cz>
2025-08-29 12:20:10 +0200

    Add base url to Vite config.

--- frontend/DataSpecificationNavigatorFrontend/vite.config.ts
+++ frontend/DataSpecificationNavigatorFrontend/vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+console.log("===== vite.config.ts =====");
+console.log("process.env.VITE_BASE_URL: " + process.env.VITE_BASE_URL);
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
